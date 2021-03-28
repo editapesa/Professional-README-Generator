4f480f6548745f71a43509ea8b13691d114d26b4
Edita Pesa <editapesa@gmail.com>
2021-03-28 14:32:51 -0400

    create questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -4,8 +4,50 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        
-    }
+       type: 'input',
+       message: 'What is the title of your project?',
+       name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please give a description explaining the project.',
+        name: 'description',
+     },
+     {
+        type: 'input',
+        message: 'What are the steps required to install the project?',
+        name: 'installation',
+     },
+     {
+        type: 'input',
+        message: 'Provide instructions, examples of use, or demo pics.',
+        name: 'usage',
+     },
+     {
+        type: 'confirm',
+        message: 'Can other developers contribute to the project? (y/N)',
+        name: 'contributing',
+     },
+     {
+        type: 'confirm',
+        message: 'Would you like to include any tests for the project? (y/N)',
+        name: 'tests',
+     },
+     {
+        type: 'list',
+        message: 'Which licenses, if any, were used?',
+        name: 'license',
+     },
+     {
+        type: 'input',
+        message: 'Please enter the GitHub username(s) to be contacted for any questions.',
+        name: 'questions',
+     },
+     {
+        type: 'input',
+        message: 'Please enter the email address to be contacted for any additional questions.',
+        name: 'questions',
+     },
 ];
 
 // TODO: Create a function to write README file
