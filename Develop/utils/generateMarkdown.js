57a74c2d8a502a953205ba0c9a91fcebf04541b4
Edita Pesa <editapesa@gmail.com>
2021-03-28 23:15:58 -0400

    change "data" to "answers" in license functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (data.license === 'MIT') {
+  if (answers.license === 'MIT') {
     return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-  }else if (data.license === 'ISC') {
+  }else if (answers.license === 'ISC') {
     return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`;
-  }else if (data.license === 'BSD 2-Clause') {
+  }else if (answers.license === 'BSD 2-Clause') {
     return `![License: BSD 2-Clause](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)`;
   }else {
     return `[ ]`; 
@@ -15,11 +15,11 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (data.license === 'MIT') {
+  if (answers.license === 'MIT') {
     return `[MIT License](https://opensource.org/licenses/MIT)`;
-  }else if (data.license === 'ISC') {
+  }else if (answers.license === 'ISC') {
     return `[ISC License](https://opensource.org/licenses/ISC)`;
-  }else if (data.license === 'BSD 2-Clause') {
+  }else if (answers.license === 'BSD 2-Clause') {
     return `[BSD 2-Clause License](https://opensource.org/licenses/BSD-2-Clause)`;
   }else {
     return `[ ]`;
@@ -29,11 +29,11 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (data.license === 'MIT') {
+  if (answers.license === 'MIT') {
     return `[This project is licensed under the MIT license.]`;
-  }else if (data.license === 'ISC') {
+  }else if (answers.license === 'ISC') {
     return `[This project is licensed under the ISC License.]`;
-  }else if (data.license === 'BSD 2-Clause') {
+  }else if (answers.license === 'BSD 2-Clause') {
     return `[This project is licensed under the BSD 2-Clause License.]`;
   }else {
     return `[ ]`;
